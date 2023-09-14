736029a6c100eff28ee766eba18c55a73c0dff7d
kalyatsabita <titakalya@gmail.com>
2023-09-14 20:33:42 +0700

    update latihan-api-sederhana

---
+++ latihan-api-sederhana/item.js
@@ -0,0 +1,22 @@
+// memanggil modul expressjs
+const express = require('express');
+
+// memanggil route/rute yang sudah dibuat di folder routes
+const userRoute = require('./routes/item.route')
+
+// set app dengan expressjs
+const app = express();
+
+// set json sebagai pertukaran data
+app.use(express.json());
+
+// set route user  di app
+app.use(userRoute)
+
+// define port untuk akses ke api
+const port = 3000;
+
+// run app
+app.listen(port, () => {
+    console.log('Server started on port : '+ port);
+});
